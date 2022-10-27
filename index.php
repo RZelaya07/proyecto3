18ffe154493f599822efe37ea872bbe8844f5f3c
RZelaya07 <ramonzelaya27@gmail.com>
2022-10-26 23:40:17 -0600

    iniciando proyecto 3

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROEYCTO 2</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <div id="contenedorPrincipal">
+        <h2>Background Color: <span id="textoColor"></span></h2>
+      
+    </div>
+    
+    <div id="contenedor2" class="flex-container">
+        <button onclick="cambiarColor()" class="button button1" id="boton1">Click me</button>
+    </div>
+    
+    <script src="js/index.js"></script>
+</body>
+</html>
